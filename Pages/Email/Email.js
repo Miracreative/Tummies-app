8091085d7ea0de71d54cc1c297f51c0650d9d978
Nana <nanalit16@gmail.com>
2024-02-01 17:56:14 +0300

    email os

--- Pages/Email/Email.js
+++ Pages/Email/Email.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {Text, View, SafeAreaView, Image, TextInput, KeyboardAvoidingView, TouchableOpacity, ImageBackground } from 'react-native';
+import {Text, View, SafeAreaView, Image, TextInput, KeyboardAvoidingView, TouchableOpacity, ImageBackground, Platform, StyleSheet, TouchableWithoutFeedback,Keyboard,   Button, } from 'react-native';
 import { RFPercentage, RFValue } from "react-native-responsive-fontsize";
 import {useTranslation} from 'react-i18next';
 import BtnButton from '../../Components/Button/Button';
@@ -28,62 +28,76 @@ export default function Email({navigation}) {
         }
       }
     return (
-        <View style={styled.container}>
+        <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+        style={styles.container}>
+          <View style={styled.container}>
              <ImageBackground
                 resizeMode='cover'
                 style={styled.back}
                 source={icons.backRedFull}>
     
         <SafeAreaView style={styled.email}>
-          
-		<View
-			style={styled.wrapper}>
-			<Header onPress={() => navigation.goBack()} isWhite={true} />
-			<Image
-				style={styled.image}
-				source={icons.headerName}/>
-            <Image 
-                style={styled.email__img}
-                source={icons.third} />
-		</View>
-        <KeyboardAvoidingView 
-            // behavior={Platform.OS =='ios' ? "padding" : "height"}
-            style={{position: 'relative', flex: .45}}>
-                
-            <Text
-                style={[styled.email__title, {fontSize: RFValue ( 22,  740)}]}>
-                E-mail
-            </Text>
-            <TextInput
-                style={styled.email__input}
-                value = {email}
-                onChangeText = {(text)=> {validateEmail(text);}}
-                onFocus={() => {setIsShowKeyboard(true);}}
-                onBlur={() => {setIsShowKeyboard(false);
-                    // setValid(validateEmail(text));
-                    // setDisable(validateEmail(text));
-                    }}
-                
-            />
-            {valid ? 
-            null : <Text style={styled.email__error}>Enter the correct e-mail</Text>
-            }
-            <View style={[styled.email__buttons, isShowKeyboard ? {marginTop: 0} : {marginTop: 'auto'}]}>
-                <BtnButton onPress={() => navigation.navigate('Gender')} title={t('continue')} buttonStyle={{backgroundColor: '#F55926',borderWidth: 2, borderColor: '#F55926', opacity: disable  ? .7 : 1, pointerEvents: disable ? 'none' : 'auto'}} textStyle={{color: 'rgba(244, 237, 225, 1)', }}/>
-                <View style={styled.email__privacy}>
-                    <Text style={styled.email__text}>By continuing, you agree to our</Text>
-                    <View style={styled.email__stroke}>
-                        <TouchableOpacity><Text style={[styled.email__term, {textDecorationLine: 'underline', textDecorationStyle: 'solid', textDecorationColor: '#0C0300'}]}>Terms</Text></TouchableOpacity><Text style={styled.email__term}> and </Text><TouchableOpacity><Text style={[styled.email__term, {textDecorationLine: 'underline', textDecorationStyle: 'solid', textDecorationColor: '#0C0300'}]}>Privacy Policy</Text></TouchableOpacity>
-                    </View>
-                    
-                </View>
-            </View>
-        </KeyboardAvoidingView>
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          <View style={styles.inner}>
+            <View
+                style={[styled.wrapper, {flex: isShowKeyboard ? .2 : 0.48}]}>
+                <Header onPress={() => navigation.goBack()} isWhite={true} />
+                <Image
+                    style={styled.image}
+                    source={icons.headerName}/>
+                    {
+                        isShowKeyboard ? null :
+                        <Image
+                        style={styled.email__img}
+                        source={icons.first} />
+                    }
         
-    </SafeAreaView>
-    </ImageBackground>
-    </View>
+            </View>
+            <View style={{flex: isShowKeyboard ? 0.8 : 0.52, justifyContent: 'flex-end', backgroundColor: 'red'}}>
+                <Text
+                    style={[styled.email__title, {fontSize: RFValue ( 22,  740)}]}>
+                    E-mail
+                </Text>
+                <TextInput
+                    style={styled.email__input}
+                    value = {email}
+                    onChangeText = {(text)=> {validateEmail(text);}}
+                    onFocus={() => {setIsShowKeyboard(true);}}
+                    onBlur={() => {setIsShowKeyboard(false);
+                        // setValid(validateEmail(text));
+                        // setDisable(validateEmail(text));
+                        }}
+                    
+                />
+                {valid ? 
+                null : <Text style={styled.email__error}>Enter the correct e-mail</Text>
+                }
+                <View style={{marginBottom: isShowKeyboard ? 5 : 20}}>
+                    <BtnButton onPress={() => navigation.navigate('Gender')} title={t('continue')} buttonStyle={{backgroundColor: '#F55926',borderWidth: 2, borderColor: '#F55926', opacity: disable  ? .7 : 1, pointerEvents: disable ? 'none' : 'auto'}} textStyle={{color: 'rgba(244, 237, 225, 1)', }}/>
+                    <BtnButton onPress={() => navigation.navigate("Auth")} title={t('logIn')} buttonStyle={{marginTop: isShowKeyboard ? 5 : 15, marginBottom: isShowKeyboard ? 5 : 20 }} textStyle={{color: 'black'}}/>
+                </View>
+
+            </View>
+           
+          </View>
+        </TouchableWithoutFeedback>
+        </SafeAreaView>
+        </ImageBackground>
+        </View>
+      </KeyboardAvoidingView>
+
     )
 
    
 }
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+    },
+    inner: {
+      flex: 1,
+      justifyContent: 'space-between',
+    },
+  });
