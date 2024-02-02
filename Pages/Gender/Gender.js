4334bbd2308357fae87050c6756c6e76120e444c
Nana <nanalit16@gmail.com>
2024-02-02 11:42:24 +0300

    check gender

--- Pages/Gender/Gender.js
+++ Pages/Gender/Gender.js
@@ -1,30 +1,29 @@
 import React, {useState} from 'react';
-import { Text, View, Image, TouchableOpacity} from 'react-native';
+import { Text, View, Image, TouchableOpacity, SafeAreaView,ImageBackground} from 'react-native';
 import {useTranslation} from 'react-i18next';
 import 'react-native-gesture-handler';
+import { RFPercentage, RFValue } from "react-native-responsive-fontsize";
 import {gen1} from './../../actions';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch} from 'react-redux';
 import BtnButton from '../../Components/Button/Button';
 import {icons} from "../../constants";
 import Header from '../../Components/Header/Header';
-import styled from "./style.scss";
+import styled from "./style.scss"; 
 export default function Gender({ navigation }) {
 
   let {t} = useTranslation();
-  const gender1 = useSelector(state => state.childrens.children1.gender);
   const [gender, setGender] = useState(null);
   const dispatch = useDispatch();
 
   return (
     
         <View style={styled.gender}>
-          <View style={styled.gender__back}>
-            <Image style={styled.gender__backImg}
-                    source={icons.back}/>
-          </View>
-            <Header style={{flex: 0.1}} onPress={() => navigation.navigate("Auth")} isButtons={false}/>
-            <View 
-              style={styled.gender__container}>
+          <ImageBackground
+            resizeMode="cover"
+            style={styled.back}
+            source={icons.backFull}>
+              <SafeAreaView style={{flex:1, justifyContent: 'space-between', alignItems: 'center'}}>
+                <Header style={{flex: 0.1}} onPress={() => navigation.goBack()} isButtons={false}/>
                 <Text style={styled.gender__title}>{t('genderTitle')}</Text>
                 <Text style={styled.gender__descr}>{t('genderDescr')}</Text>
                 <TouchableOpacity style={styled.gender__iconContainer}
@@ -42,8 +41,9 @@ export default function Gender({ navigation }) {
                 <BtnButton onPress={() => {dispatch(gen1(gender))
                                             navigation.navigate('Name')
                                           console.log('name')}
-                                            } title={t('next')} buttonStyle={{backgroundColor: '#F55926',borderWidth: 2, borderColor: '#F55926', opacity: gender ? 1 : 0.7, pointerEvents: gender ? 'auto' : 'none'}} textStyle={{color: 'rgba(244, 237, 225, 1)', }}/>
-            </View>
+                                            } title={t('next')} buttonStyle={{backgroundColor: '#F55926',borderWidth: 2, borderColor: '#F55926', opacity: gender ? 1 : 0.7, pointerEvents: gender ? 'auto' : 'none', marginBottom: 40}} textStyle={{color: 'rgba(244, 237, 225, 1)', }}/>
+              </SafeAreaView>
+          </ImageBackground>
     </View>
   );
 }
